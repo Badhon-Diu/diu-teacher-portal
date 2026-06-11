a49478e9d1fc93c82430d44115051ebee78ba936
Habibul Islam <habibulislam6862@gmail.com>
2026-06-11 22:43:13 +0600

    Update student data: replace MD. Tanvir Ahmed with Habibul Islam

--- src/data/studentsData.js
+++ src/data/studentsData.js
@@ -4,5 +4,6 @@ export const initialStudents = [
   { id: 3, studentId: '251-15-128', name: 'Rafiq Ahmed', attm: 2, q1: '', q2: '', q3: '', presn: '', assign: '', mt: '', mtIm: '', final: '', abs: false, wh: false },
   { id: 4, studentId: '251-15-138', name: 'Fatema Khatun', attm: 4, q1: '', q2: '', q3: '', presn: '', assign: '', mt: '', mtIm: '', final: '', abs: false, wh: false },
   { id: 5, studentId: '251-15-146', name: 'MD. Sohel Rana', attm: 1, q1: '', q2: '', q3: '', presn: '', assign: '', mt: '', mtIm: '', final: '', abs: false, wh: false },
-  { id: 6, studentId: '0242220005101707', name: 'Habibul Islam', attm: 1, q1: '', q2: '', q3: '', presn: '', assign: '', mt: '', mtIm: '', final: '', abs: false, wh: false },
+  { id: 6, studentId: '251-15-045', name: 'MD. Tanvir Ahmed', attm: 3, q1: '', q2: '', q3: '', presn: '', assign: '', mt: '', mtIm: '', final: '', abs: false, wh: false },
+  { id: 7, studentId: '0242220005101707', name: 'Habibul Islam', attm: 1, q1: '', q2: '', q3: '', presn: '', assign: '', mt: '', mtIm: '', final: '', abs: false, wh: false },
 ]
